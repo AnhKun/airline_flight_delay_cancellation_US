659346d3e67a97e0c8861f764a96dfd3592f85e1
Anh Nguyen <nganhh014@gmail.com>
2022-02-11 13:13:13 +0200

    insert into tables

--- sql_queries.py
+++ sql_queries.py
@@ -80,3 +80,84 @@ CREATE TABLE IF NOT EXISTS airline_fact (
 """
 
 # insert into tables
+aircraft_dim_insert = """
+INSERT INTO aircraft_dim (
+    c_aircrafe,
+    c_airline
+)
+VALUES (%s, %s)
+"""
+
+airline_dim_insert = """
+INSERT INTO airline_dim (
+    c_airline,
+    airline_name 
+)
+VALUES (%s, %s)
+"""
+
+cancel_dim_insert = """
+INSERT INTO cancel_dim (
+    c_cancel,
+    cancel_des 
+)
+VALUES (%s, %s)
+"""
+
+delay_dim_insert = """
+INSERT INTO delay_dim (
+    delay_group,
+    time_range_minute
+)
+VALUES (%s, %s)
+"""
+
+distance_dim_insert = """
+INSERT INTO distance_dim (
+    distance_group,
+    distance_range_mile
+)
+VALUES (%s, %s)
+"""
+
+port_loc_dim_insert = """
+INSERT INTO port_loc_dim (
+    c_port,
+    city_name,
+    c_state
+)
+VALUES (%s, %s, %s)
+"""
+
+state_dim_insert = """
+INSERT INTO state_dim (
+    c_state,
+    state_name
+)
+VALUES (%s, %s)
+"""
+
+airline_fact_insert = """
+INSERT INTO airline_fact (
+    flight_date,
+    c_airline,
+    flight_num,
+    c_aircraft,
+    origin,
+    dest,
+    schedule_arr_time,
+    actual_arr_time,
+    arr_delay_group,
+    schedule_dep_time,
+    actual_dep_time,
+    dep_delay_group,
+    distance_group,
+    c_cancel 
+)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+"""
+
+# query list
+create_tables_query = [aircraft_dim_create, airline_dim_create, cancel_dim_create, delay_dim_create, distance_dim_create, port_loc_dim_create, state_dim_create]
+
+drop_tables_query = [aircraft_dim_drop, airline_dim_drop, cancel_dim_drop, delay_dim_drop, distance_dim_drop, port_loc_dim_drop, state_dim_drop]
